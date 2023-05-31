18e95ad61771f1cc36e25df4fe86edfb2a0a8389
r3l4x1n <nfzapataca@cesde.net>
2023-05-31 18:09:26 -0500

    Solucion Ejercicios 01

--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -11,12 +11,21 @@ function tienenMismaLongitud(str1, str2) {
    // Retorna true si los dos strings tienen la misma longitud.
    // De lo contrario, retorna false.
    // Tu código:
+   if(str1.length === str2.length){
+      return true;
+   }else{
+      return false;
+   }
 }
+console.log (tienenMismaLongitud ("miguel", "humberto"));
 
 function menosQueNoventa(num) {
    // Retorna true si el argumento "num" es menor que noventa.
    // De lo contrario, retorna false.
    // Tu código:
+   if (num < 90){
+      return true;
+   }
 }
 
 function mayorQueCincuenta(num) {
@@ -29,12 +38,22 @@ function esPar(num) {
    // Retorna true si "num" es par.
    // De lo contrario, retorna false.
    // Tu código:
+   if(num % 2 === 0){
+      return true;
+   }else{
+      return false;
+   }
 }
 
 function esImpar(num) {
    // Retorna true si "num" es impar.
    // De lo contrario, retorna false.
    // Tu código:
+   if(num % 2 !== 0){
+      return true;
+   }else{
+      return false;
+   }
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
